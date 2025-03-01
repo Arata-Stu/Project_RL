d52499dc1eca7c770f6ee6d35ab280559e62fa68
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-03-02 04:22:45 +0900

    update

--- src/models/VAE/base.py
+++ src/models/VAE/base.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from typing import Tuple
 from src.utils.timers import Timer as Timer
 # from src.utils.timers import TimerDummy as Timer
@@ -29,3 +30,12 @@ class BaseVAE(nn.Module):
         z = self.latent(mu, logvar)
         out = self.decode(z)
         return out, mu, logvar
+    
+    def vae_loss(self, out, y, mu, logvar):
+        BCE = F.binary_cross_entropy(out, y, reduction="sum")
+        KL = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+        return BCE + KL, BCE, KL
+    
+    def get_latent_size(self):
+        
+        return self.latent_size
